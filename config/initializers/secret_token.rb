705deee21b9c32968587eb99ee12dcf00b4a4a2d
Jun Lim <junyuen@gmail.com>
2013-07-30 23:29:19 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blindpirates::Application.config.secret_token = '701b6a10a09bf0f62d4f288aa62c0d233a91a9ef948cce28056d78713ffbceafd0e64062d444a09178f64b9fc37453a5dd0e76452249f7a159945738fcafce23'
